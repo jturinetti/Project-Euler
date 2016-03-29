940900d461cd225677d21696a98fbd594fad3ee1
Jeremy <jeremy.turinetti@gmail.com>
2016-03-29 00:09:19 -0500

    additional solutions

--- euler.py
+++ euler.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from math import sqrt
 
 #helpers
 def factorial(num):
@@ -32,6 +33,27 @@ def prime_numbers(num_to_find):
             prime_numbers.append(counter)
         
         if (len(prime_numbers) >= num_to_find):
+            done = True
+            
+        counter = counter + 2
+    return prime_numbers
+
+def prime_numbers_below(num):
+    prime_numbers = [2]    
+    done = False
+    counter = 3
+    while (not done):        
+        isPrime = True
+        for pn in prime_numbers:
+            if (counter % pn == 0):
+                isPrime = False
+
+        if (isPrime):
+            prime_numbers.append(counter)
+            print("prime numbers found: ", len(prime_numbers))
+            print("last prime number found: ", counter)
+        
+        if (counter > num):
             done = True
             
         counter = counter + 2
@@ -86,7 +108,96 @@ def largest_product_in_series():
             starting_index = starting_index + 1
             
     return largest_product
-        
 
+#euler problem 9
+def pythag_triplet():
+    actual_a = 0
+    actual_b = 0
+    done = False
+    for a in range(1, 1000):
+        for b in range(1, 1000):            
+            output = (a + b + sqrt(a**2 + b**2))
+            if (output == 1000):
+                actual_a = a
+                actual_b = b
+                done = True
+            if (done):
+                break
+        if (done):
+            break
+    c = sqrt(actual_a ** 2 + actual_b ** 2)
+    print(actual_a)
+    print(actual_b)
+    print(c)
+    return actual_a * actual_b * c
+
+#euler problem 10
+def summation_of_primes():
+    #THIS TAKES A LONG TIME, NEED TO OPTIMIZE
+    primes_list = prime_numbers_below(2000000)
+    return sum(primes_list)
+
+#euler problem 11
+def largest_product_in_grid():
+    grid = []
+    grid_string =  '''
+08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
+81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
+22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
+24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
+32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
+67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
+24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
+21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
+78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
+16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
+86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
+19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
+04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
+88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
+04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
+20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
+20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
+01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48
+'''.strip()
+    for line in grid_string.splitlines():
+        grid.append([int(number) for number in line.split()])
+
+    largest_product = 0
+    x = 0
+    y = 0
+    while (x < (len(grid))):
+        while (y < (len(grid[x]))):            
+            #horizontal
+            if (y < len(grid[x]) - 3):
+                #print ("horizontal mult: {0} * {1} * {2} * {3}".format(grid[x][y], grid[x][y+1], grid[x][y+2], grid[x][y+3]))
+                product = grid[x][y] * grid[x][y+1] * grid[x][y+2] * grid[x][y+3]
+                if (product > largest_product):
+                    largest_product = product
+            if (x < (len(grid) - 3)):
+                #vertical
+                #print ("vert mult: {0} * {1} * {2} * {3}".format(grid[x][y], grid[x+1][y], grid[x+2][y], grid[x+3][y]))
+                product = grid[x][y] * grid[x+1][y] * grid[x+2][y] * grid[x+3][y]                
+                if (product > largest_product):
+                    largest_product = product
+            if (x < (len(grid) - 3) and y < len(grid[x]) - 3):
+                #diagonal to the right
+                #print ("diag mult: {0} * {1} * {2} * {3}".format(grid[x][y], grid[x+1][y+1], grid[x+2][y+2], grid[x+3][y+3]))
+                product = grid[x][y] * grid[x+1][y+1] * grid[x+2][y+2] * grid[x+3][y+3]                
+                if (product > largest_product):
+                    largest_product = product
+            if (x < (len(grid) - 3) and (y - 3) > 0):
+                #diagonal to the left
+                #print ("diag mult: {0} * {1} * {2} * {3}".format(grid[x][y], grid[x+1][y-1], grid[x+2][y-2], grid[x+3][y-3]))
+                product = grid[x][y] * grid[x+1][y-1] * grid[x+2][y-2] * grid[x+3][y-3]
+                if (product > largest_product):
+                    largest_product = product
+            y = y + 1
+        y = 0        
+        x = x + 1
+    return largest_product
+        
 # MAIN
-print(largest_product_in_series())
+print(largest_product_in_grid())
